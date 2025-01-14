59b5c6d35a088334c2b479e4f7e3f075c4f0c93c
David Antón Gea <d.antongea@um.es>
2025-01-14 20:23:31 +0100

    doc acutalizado

--- Angular/src/app/environments/environment.ts
+++ Angular/src/app/environments/environment.ts
@@ -6,12 +6,12 @@ export const environment = {
     preguntasApi:'http://25.49.55.101:8385'
     */
     
-     userApi: 'http://127.0.0.1:8384',
-     preguntasApi: 'http://127.0.0.1:8385'
+    // userApi: 'http://127.0.0.1:8384',
+    // preguntasApi: 'http://127.0.0.1:8385'
     
     
-   //userApi: 'https://tfg-usuarios-rest.onrender.com',
-   //preguntasApi: 'https://tfg-preguntas-rest.onrender.com'
+   userApi: 'https://tfg-usuarios-rest.onrender.com',
+   preguntasApi: 'https://tfg-preguntas-rest.onrender.com'
 
    
 };
