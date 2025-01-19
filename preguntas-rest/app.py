e363eea4cf5aee1277eba8eb20acceddf787856c
David Antón Gea <d.antongea@um.es>
2025-01-19 20:25:18 +0100

    cambios de ultima hora

--- preguntas-rest/app.py
+++ preguntas-rest/app.py
@@ -58,7 +58,7 @@ def index():
 	
 @app.after_request
 def after_request(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Origin"] = "https://caballoloko2002.github.io"
     response.headers["Access-Control-Allow-Credentials"] = "true"
     response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
     response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization,enctype"
